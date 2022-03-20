2954d865b0baf57a24ca5a85764d3b9e654db9ca
payam <payamase@gmail.com>
2022-03-19 20:26:05 -0700

    add EC2 modul and its TF plan

--- practice/variables.tf
+++ practice/variables.tf
@@ -18,3 +18,19 @@ variable "s3_bucket_acl" {
   description = "The ACL for the S3 bucket."
   default     = null
 }
+
+# EC2
+variable "ec2_ami" {
+  description = "EC2 Amazon Machine Images (AMI) ID"
+  default     = null
+}
+
+variable "ec2_instance_type" {
+  description = "EC2 instance type"
+  default     = null
+}
+
+variable "ec2_tag_name" {
+  description = "EC2 tag name"
+  default     = null
+}
