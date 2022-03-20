061e66ec5ee9de46c48991a3d742b10574705f87
payam <payamase@gmail.com>
2022-03-19 19:58:23 -0700

    use remote backend instead of local

--- practice/backend.tf
+++ practice/backend.tf
@@ -1,5 +1,3 @@
-# The block below configures Terraform to use the 'remote' backend with Terraform Cloud.
-# For more information, see https://www.terraform.io/docs/backends/types/remote.html
 terraform {
   required_providers {
     aws = {
@@ -10,14 +8,15 @@ terraform {
 
   required_version = ">= 1.1.7"
 
-  # Terraform Cloud -- but it has a bug in 1.1.7 version of terraform
-  # backend "remote" {
-  #   hostname     = "app.terraform.io"
-  #   organization = "pymkdb"
+  # Terraform Cloud as the remote backend
+  # Execution mode is set to "local" in Terrafrom Cloud setting page for this workspace
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "pymkdb"
 
-  #   workspaces {
-  #     name = "sandbox"
-  #   }
-  # }
+    workspaces {
+      name = "sandbox"
+    }
+  }
 
 }
